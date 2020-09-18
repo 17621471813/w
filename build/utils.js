a7b4395131075b94a0bea9614e5c877e54c222c3
Your Name <193770305@qq.com>
2020-09-19 00:07:48 +0800

    cong

--- build/utils.js
+++ build/utils.js
@@ -48,7 +48,7 @@ exports.cssLoaders = function (options) {
       return ExtractTextPlugin.extract({
         use: loaders,
         fallback: 'vue-style-loader',
-        publicPath: '../../'
+        publicPath: '../../dist/index.html'
       })
     } else {
       return ['vue-style-loader'].concat(loaders)
